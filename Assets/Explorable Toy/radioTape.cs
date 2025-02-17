dc2eebc067f5f94d41216e6c93ae43a75fcb7b02
Xiaofan Bai <baixia@shernet.sheridancollege.ca>
2025-02-16 23:13:58 -0500

    I add the timer and try to make the slider too but still have some problem

---
+++ Assets/Explorable Toy/radioTape.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class radioTape : MonoBehaviour
+{
+    public GameObject rectangle; 
+    public Slider verticalSlider; 
+    public float rectangleMoveRange = 5f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rectangle != null)
+        {
+            float newX = -rectangleMoveRange * verticalSlider.value; 
+            rectangle.transform.position = new Vector3(newX, rectangle.transform.position.y, rectangle.transform.position.z);
+        }
+    }
+}
