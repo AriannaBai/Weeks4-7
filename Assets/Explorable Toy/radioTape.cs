f6f0387b96b7d1de83c5cb07a575105a179964cc
Xiaofan Bai <baixia@shernet.sheridancollege.ca>
2025-02-19 21:17:02 -0500

    added comments to radiotape script.

--- Assets/Explorable Toy/radioTape.cs
+++ Assets/Explorable Toy/radioTape.cs
@@ -5,9 +5,10 @@ using UnityEngine.UI;
 
 public class radioTape : MonoBehaviour
 {
-    public GameObject rectangle; 
-    public Slider verticalSlider; 
-    public float rectangleMoveRange = 50f; 
+    public GameObject rectangle; //set a game object for the tape.
+    public Slider verticalSlider; //this is a UI setting to activate the slider function in unity.
+    public float rectangleMoveRange = 50f; //I set an initial range for the tape. It is like the speed when sliding the slider.
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +20,12 @@ public class radioTape : MonoBehaviour
     {
         if (rectangle != null)
         {
-            float newX = -rectangleMoveRange * verticalSlider.value; 
-            rectangle.transform.position = new Vector3(newX, rectangle.transform.position.y, rectangle.transform.position.z);
+            float newX = -rectangleMoveRange * verticalSlider.value;
+            //calculate the new position based on the slider value.
+            //I multiply this two value to determine how far the rectangle(tape) should move within it's range.
+            //I set it to negative to make sure the rectangle moves in the opposite direction. 
+
+            rectangle.transform.position = new Vector3(newX, rectangle.transform.position.y, rectangle.transform.position.z);//apply the new position while keeping y and z unchanged. 
         }
     }
 }
